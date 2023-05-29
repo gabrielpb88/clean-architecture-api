55f581aaf77f9cac6f074963395f3c1afce020f7
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-05-29 16:36:09 -0300

    refactor: move sut creation to a factory method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -2,6 +2,10 @@ import { MongoHelper } from '../helper/mongo-helper'
 import { LogMongoRepository } from './log'
 import { Collection } from 'mongodb'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection
   beforeAll(async () => {
@@ -18,7 +22,7 @@ describe('Log Mongo Repository', () => {
   })
 
   test('Should create a log error on success', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
