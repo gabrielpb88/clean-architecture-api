be6e07c183644214c8ec79fbd544c8c07a3224e3
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-05-31 13:36:39 -0300

    test: ensure LoginController returns 400 when no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -24,4 +24,15 @@ describe('Login UseCase', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
+
+  test('Should return 400 when password is not provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
 })
